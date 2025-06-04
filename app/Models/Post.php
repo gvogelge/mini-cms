b8d5b790968acc44f2e1beedd801ad261cf1a29c
gvogelge <127736254+gvogelge@users.noreply.github.com>
2025-06-04 18:10:56 +0200

    Add relationships and fillable fields to Post and User models

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,7 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property int $id
+ * @property string $title
+ * @property string $content
+ * @property string|null $image_path
+ * @property \App\Models\User $user
+ */
 class Post extends Model
 {
-    //
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    protected $fillable = [
+        'title',
+        'content',
+        'image_path',
+        'user_id',
+    ];
 }
